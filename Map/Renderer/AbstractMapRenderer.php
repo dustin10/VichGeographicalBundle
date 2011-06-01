3e9863a51f87bc1c381df12f031f01190b65e7c2
H. Westphal <westphal@secure-net-concepts.de>
2011-06-01 19:35:39 +0200

    Initial Leaflet support.

--- Map/Renderer/AbstractMapRenderer.php
+++ Map/Renderer/AbstractMapRenderer.php
@@ -13,6 +13,43 @@ use Vich\GeographicalBundle\Map\Renderer\MapRendererInterface;
 abstract class AbstractMapRenderer implements MapRendererInterface
 {
     /**
+     * @var array
+     */
+    protected $options;
+
+    /**
+     * Constructor
+     *
+     * @param array $options
+     */
+    public function __construct(array $options = array())
+    {
+        $this->options = $options;
+    }
+
+    /**
+     * Returns the value of an option.
+     *
+     * @param string $name
+     * @return mixed
+     */
+    public function getOption($name)
+    {
+        return isset($this->options[$name]) ? $this->options[$name] : null;
+    }
+
+    /**
+     * Sets the value of an option.
+     *
+     * @param string $name
+     * @param mixed $value
+     */
+    public function setOption($name, $value)
+    {
+        $this->options[$name] = $value;
+    }
+
+    /**
      * Renders the Map.
      * 
      * @param Vich\GeographicalBundle\Map\Map $map The map
