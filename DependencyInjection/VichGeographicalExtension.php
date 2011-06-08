902e3042a93544a60b9b5a069aa4dd5279f6806a
Dustin Dobervich <ddobervich@gmail.com>
2011-06-08 15:04:48 -0500

    cosmetic change to extension class

--- DependencyInjection/VichGeographicalExtension.php
+++ DependencyInjection/VichGeographicalExtension.php
@@ -61,6 +61,7 @@ class VichGeographicalExtension extends Extension
         if (null !== $config['leaflet']['api_key']) {
             $rendererOptions['leaflet_api_key'] = $config['leaflet']['api_key'];
         }
+        
         $container->setParameter('vich_geographical.map_renderer.options', $rendererOptions);
 
         $container->setParameter('vich_geographical.map_renderer.class', $config['class']['map_renderer']);
